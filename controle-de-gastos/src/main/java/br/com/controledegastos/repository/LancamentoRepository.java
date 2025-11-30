01d0234c4292e27336ce4cc09b873b128a17a959
WnnJunior <112138394+WnnJunior@users.noreply.github.com>
2025-11-30 01:46:07 -0300

    Add files via upload

---
+++ controle-de-gastos/src/main/java/br/com/controledegastos/repository/LancamentoRepository.java
@@ -0,0 +1,10 @@
+package br.com.controledegastos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.controledegastos.model.Lancamento;
+
+@Repository
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+}
